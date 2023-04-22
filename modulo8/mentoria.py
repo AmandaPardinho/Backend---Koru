931d723ee4167741f4fbf1bd0c9bb739432a6b7b
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-22 18:57:40 -0300

    Criação do método to_dict

--- modulo8/mentoria.py
+++ modulo8/mentoria.py
@@ -7,3 +7,13 @@ class Mentoria:
         self.mentor = mentor
         self.mentorando = mentorando
         self.data = data
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "id_mentor": self.mentor.id,
+            "mentor": self.mentor.to_dict(),
+            "id_mentorando": self.mentorando.id,
+            "mentorando": self.mentorando.to_dict(),
+            "data": self.data
+        }
