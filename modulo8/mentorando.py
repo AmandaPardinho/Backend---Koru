598c899d2e20c62ca017597b27537e784bb87e71
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-22 19:43:22 -0300

    Implementação do método save

--- modulo8/mentorando.py
+++ modulo8/mentorando.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Mentorando:
     def __init__(self, nome: str, linkedin: str, id = None):
         self.id = id
@@ -10,3 +12,17 @@ class Mentorando:
             "nome": self.nome,
             "linkedin": self.linkedin
         }
+    
+    # Este método da classe Mentorandos possui novamente o 'cursor', pois será necessário chamar o lastrowid e isso só é possível com ele presente
+    def save(self, db_connection: sqlite3.Connection):
+        if self.id is None:
+            query = "INSERT INTO mentorandos (nome_mentorando, linkedin_mentorando) VALUES (?, ?)"
+            cursor = db_connection.cursor()
+            cursor.execute(query, (self.nome, self.linkedin))
+            self.id = cursor.lastrowid
+        else:
+            query = "UPDATE mentorandos SET nome_mentorando = ?, linkedin_mentorando = ? WHERE id_mentorando = ?"
+            cursor = db_connection.cursor()
+            cursor.execute(query, (self.nome, self.linkedin, self.id))
+        db_connection.commit()
+        db_connection.close()
