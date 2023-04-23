eef659fea398393d2926ea67d411214f34021397
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-23 19:38:50 -0300

    Correção do código

--- modulo8/mentorando.py
+++ modulo8/mentorando.py
@@ -22,8 +22,7 @@ class Mentorando:
             self.id = cursor.lastrowid
         else:
             query = "UPDATE mentorandos SET nome_mentorando = ?, linkedin_mentorando = ? WHERE id_mentorando = ?"
-            cursor = db_connection.cursor()
-            cursor.execute(query, (self.nome, self.linkedin, self.id))
+            db_connection.execute(query, (self.nome, self.linkedin, self.id))
         db_connection.commit()
         db_connection.close()
 
@@ -32,8 +31,8 @@ class Mentorando:
         query = "DELETE FROM mentorandos WHERE id_mentorando = ?"
         cursor = db.cursor()
         cursor.execute(query, (self.id, ))
-        cursor.commit()
-        cursor.close()
+        db.commit()
+        db.close()
 
     @staticmethod
     def get_by_id(id: int, db: sqlite3.Connection):
